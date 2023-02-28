b9fb5067c36c46518ab81324dbaf4c18dab726a6
argsoez <123755135+argsoez@users.noreply.github.com>
2023-02-28 03:40:57 -0300

    Proyecto hecho con HTML, CSS, Boopstrap y Java Script.

---
+++ javascript.js
@@ -0,0 +1,40 @@
+ //quiebre 1
+ const ipersonaljs = document.querySelector(".ipersonaljs");
+
+ ipersonaljs.addEventListener("mouseenter", () =>{ipersonaljs.style.backgroundColor = "#FFEA20";
+ });
+ 
+ ipersonaljs.addEventListener("mouseleave", () => {
+    ipersonaljs.style.backgroundColor = "#FC7300 ";
+ });
+ 
+  //quiebre 2
+  const front = document.querySelector(".front");
+ 
+  front.addEventListener("mouseenter", () =>{front.style.backgroundColor = "#9CFF2E";
+  });
+  
+  front.addEventListener("mouseleave", () => {
+      front.style.backgroundColor = "#BFDB38";
+  });
+ 
+ //quiebre 3
+ const back = document.querySelector(".back");
+ 
+ back.addEventListener("mouseenter", () =>{back.style.backgroundColor = "#9CFF2E";
+ });
+ 
+ back.addEventListener("mouseleave", () => {
+     back.style.backgroundColor = "#BFDB38";
+ });
+ 
+ 
+ //quiebre 4
+ const contacto = document.querySelector(".contacto");
+ 
+ contacto.addEventListener("mouseenter", () =>{contacto.style.backgroundColor = "#000";
+ });
+ 
+ contacto.addEventListener("mouseleave", () => {
+    contacto.style.backgroundColor = "#050505";
+ });
